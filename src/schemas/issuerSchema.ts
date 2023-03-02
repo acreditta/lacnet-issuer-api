4cf7f3b73f1f138dc0b85e93e4f1d24a42bc8221
David-B-A <dabelloa@unal.edu.co>
2023-03-02 10:59:58 -0500

    Fix/Fixing shcemas names (from users to issuers)

--- src/schemas/issuerSchema.ts
+++ src/schemas/issuerSchema.ts
@@ -5,22 +5,20 @@ const did = Joi.string();
 const privateKey = Joi.string();
 const id = Joi.number().integer().min(1);
 
-const createIssuerSchema = Joi.object({
+export const createIssuerSchema = Joi.object({
     id: id.required(),
     name: name.required(),
 });
 
-const updateIssuerSchema = Joi.object({
+export const updateIssuerSchema = Joi.object({
     name: name,
 });
 
-const getIssuerSchema = Joi.object({
+export const getIssuerSchema = Joi.object({
     id: id.required(),
     did: did.required()
 });
 
-const indexIssuerSchema = Joi.object({
+export const indexIssuerSchema = Joi.object({
     id: id
 });
-
-export { createIssuerSchema, updateIssuerSchema, getIssuerSchema, indexIssuerSchema };
